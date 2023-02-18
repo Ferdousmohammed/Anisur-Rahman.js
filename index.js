0acc9fc09e5ba8dc325cceadcd35c1aa72f52eb7
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-02-18 20:14:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+/*var num = true;
+console.log(typeof(true));
+
+
+
+var fullname = "Ferdous Mohammed";
+document.write(fullname + " is my name </br> ")
+
+var text1 = 'Bangladesh';
+var text2 = ' is beautiful country';
+var text = text1.concat(text2);
+document.write(text);
+
+
+var ko = "bangla";
+ko = ko.length  ;
+document.write(ko);*/
+
+
+
+
+// দুটি সংখ্যার ভালোবাসা
+
+/*var num1 =  prompt("enter your name : ");
+var num2 = prompt("enter your father name :");
+num1 = parseInt(num1, 10);
+num2 =  parseFloat(num2, 10);
+
+
+var sum, sub
+sum = (num1 + num2);
+document.write("increase =" + sum+"<br>");
+
+sub = (num1 - num2);
+document.write("deeceress =" + sub);*/
+
+
+/*var base = prompt('enter name value =');
+var height =  prompt('enter love value = ');
+
+var area = base * height;
+document.write('Area = ' + area);*/
+
+// farenhit to celcious
+
+/*var farenhit = parseInt(prompt("input Degree = "));
+var celcious = (farenhit - 32) * (5/9);
+
+document.write("celcious =" + celcious);*/
+
+// MORNING WORK   MAKE IT CELSIOUS TO FARENHIT
+
+
+// var celcious = parseInt(prompt("Enter your Farenhit value :"));
+// var farenhit = (celcious * 9/5) + 32;
+// document.write(farenhit);
+
+
+// DOLLER TO BDT CONVERSIONB
+
+
+// var doller =  parseInt(prompt("Enter doller : ")); 
+// var bdt = doller * 105.83;
+// document.write(bdt);
+
+
+var exam = parseFloat(prompt("Enter your Number: "));
+
+// var result = exam + 2;
+// document.write(result);
+
+if (exam >= 80)
+    console.log("A+");
+
+    else if (exam >= 70)
+    console.log("A");
+
+    else if  (exam >= 60)
+    console.log("b");
+
+    else if  (exam >= 50)
+    console.log("c");
+
+
+    else if (exam >= 40)
+    console.log("d");
+
+    else 
+    console.log("F");
+
+
